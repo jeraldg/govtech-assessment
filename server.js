eab1b08ef6b3419de35d662644322b4e22c9e6ea
Jerald <jeraldgantc@gmail.com>
2019-03-23 13:56:49 +0800

    Constructed the first endpoint and added mysql library

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json()); 
 
-app.get('/', (req, res) => {
-  res.send('Hello Express')
+app.post('/api/register', (req, res) => {
+  const { students, teacher} = req.body;
+
+  if (!students || !teacher) return;
+
+  res.sendStatus(204);
 });
 
 app.listen(process.env.PORT || 3000)
